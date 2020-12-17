baeffe3bfb5c685b73c84dd40c576187ca55b123
nathanhester-depaul <nhester@depaul.edu>
2020-12-16 17:44:18 -1000

    changed primary color and made login dialog

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,50 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+    <q-card dark
+      bordered>
+      <img
+        alt="MovieMatcher"
+        src="~assets/MMHeader2.png"
+      >
+      <br>
+      <q-btn
+        class="full-width"
+        color="primary"
+        label="Enter"
+        glossy
+      @click="loginDialog = true"/>
+      <q-dialog
+        v-model="loginDialog"
+        full-width
+        full-height
+      >
+        <q-card style="width: 300px">
+          <q-card-section>
+            <div class="text-h6">Login</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            enter your username and password.
+          </q-card-section>
+          <q-card-section class="login-form">
+            <q-input standout="bg-teal text-white" v-model="text" label="Username" />
+            <q-input standout="bg-teal text-white" v-model="text" label="Password" />
+          </q-card-section>
+          <q-card-actions align="right" class="bg-white text-teal">
+            <q-btn flat label="OK" v-close-popup />
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
+    </q-card>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+    data () {
+      return {
+          loginDialog: false
+      }
+    }
 }
 </script>
